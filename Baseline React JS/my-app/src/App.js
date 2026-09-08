4c7d9f9cf7047a7641d5cac977b9d2e5f3124e51
Rohan belsare <rohanbelsare113@gmail.com>
2026-09-08 20:46:39 +0530

    React JFX

--- Baseline React JS/my-app/src/App.js
+++ Baseline React JS/my-app/src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import TodoList from './component/todoList';
+import Profile from './component/profile';
+import  PackingList from './component/PackagingList';
+import List from './component/List';
 
 
 
@@ -9,6 +12,11 @@ function App(){
     <div className="App">
       {/* Rendering our newly imported component */}
       <TodoList />
+      <hr />
+      <Profile />
+      <PackingList />
+      <List />
+
     </div>
   )
 } 
