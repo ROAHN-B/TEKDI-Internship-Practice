3bb0f6914d39d5cc3d079a8031cb10b75d6c24db
Rohan belsare <rohanbelsare113@gmail.com>
2026-09-11 22:04:09 +0530

    completed Lists,Hooks

--- Baseline React JS/my-app/src/App.js
+++ Baseline React JS/my-app/src/App.js
@@ -13,6 +13,7 @@ import IfStatement from './component/IfStatement';
 import Guest from './component/guest';
 import React, { Component } from 'react';
 import LoginExp from './component/LoginExp';
+import ListExp from './component/ListExp';
 
 // function App(){
   
@@ -49,26 +50,59 @@ export default class App extends Component{
     //   return <IfStatement />
     // }
     // return <Guest />
-    state={
-      isLoggedIn:false
-    };
+    // state={
+    //   isLoggedIn:false
+    // };
 
-    clickLogin = () =>{
-      this.setState({isLoggedIn:true});
-    }
-    clickLogout = () =>{
-      this.setState({isLoggedout:false});
-    }
+    // clickLogin = () =>{
+    //   this.setState({isLoggedIn:true});
+    // }
+    // clickLogout = () =>{
+    //   this.setState({isLoggedout:false});
+    // }
+
+    // render(){
+    //   const isLoggedIn = this.state.isLoggedIn;
+    //   return (
+    //     <div>
+    //     {isLoggedIn ? <LoginExp name="Rohan" clickData={this.clickLogout} /> : <Guest clickData={this.clickLogin} />}
+    //   </div>
+    //   )
+    // }
+    // state = {
+    //   Users: [
+    //     {id : 101, name:"Rohan", password:"j43ik65hwh"},
+    //     {id : 102, name:"Rohit", password:"ckdv89dvko"},
+    //     {id : 103, name:"Ramesh",password:"kjnwuiod9"},
+    //     {id : 104, name:"Rakesh",password:"alknvd87d"},
+    //     {id : 105, name:"Rohini",password:"kjndsvods9"}
+    //   ]
+    // }
+    // render (){
+    //   const newUsers = this.state.Users.map((user) => {
+    //     console.log(user);
+    //     return (
+    //     <h1 key={user.id}>
+    //       ID:{user.id} Name:{user.name} Password:{user.password}
+    //     </h1>        
+    //   )
+    //   });
+    //   return (
+    //     <div>
+    //       {newUsers}
+    //     </div>
+    //   )
+    // }
 
     render(){
-      const isLoggedIn = this.state.isLoggedIn;
+      const arr =this.props.numbers;
+      const newArr = arr.map((nums) => {
+        return <ListExp key={nums} value={nums} />
+      })
       return (
         <div>
-        {isLoggedIn ? <LoginExp name="Rohan" clickData={this.clickLogout} /> : <Guest clickData={this.clickLogin} />}
-      </div>
+          {newArr}
+        </div>
       )
     }
-
   }
-
-
