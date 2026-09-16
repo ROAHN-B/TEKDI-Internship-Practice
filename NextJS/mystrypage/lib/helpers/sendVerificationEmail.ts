18e59aa8770bc26e9094514d70221ddae96fdadc
Rohan belsare <rohanbelsare113@gmail.com>
2026-09-16 23:24:16 +0530

    signup page , resend

---
+++ NextJS/mystrypage/lib/helpers/sendVerificationEmail.ts
@@ -0,0 +1,24 @@
+import {resend} from '@/lib/resend';
+import VerificationEmail from '@/Emails/VerificationEmail';
+import { ApiResponse } from '@/Types/ApiResponse';
+
+export async function sendVerificationEmail(
+    email:string,
+    username:string,
+    VerifyCode:string
+):Promise<ApiResponse>{
+    try{
+        await resend.emails.send({
+            from:"onbording@resend.dev",
+            to:email,
+            subject:"Mystry messsage | Verification Code",
+            react:VerificationEmail({username, otp:VerifyCode}),
+        });
+        return {success: true, message:'Verification emil sent successfully'}
+    }
+    catch(emailError){
+        console.error("Email sending verification email",emailError,emailError)
+        return {success: false, message:'failed to send verification email'}
+
+    }
+}
